75509ce16caf0319902b9aabc5020d5c061e0a14
nd <225004051@sastra.ac.in>
2024-05-17 22:26:51 +0530

    ff

--- render/urls.py
+++ render/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.indexs, name='index'),
     path('/handle',views.handle_button, name='handle_button'),
 
 ]
